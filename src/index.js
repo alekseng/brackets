f295f011f213d5345f60b30634af6761e133323e
alekseng <alexvdche@mail.ru>
2023-08-11 15:04:17 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrayFromStr = str;
+  const brackets = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brackets.push(bracketsConfig[i][0] + bracketsConfig[i][1])
+  }
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      arrayFromStr = arrayFromStr.replace(brackets[j], '');
+      if (arrayFromStr.length == 0) {
+        return true
+      }
+    }
+  }
+  return false
 }
